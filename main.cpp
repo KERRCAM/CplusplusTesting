52eb205c4776340b00e3fbced3a4a8647dc98c81
KERRCAM <kerrcameron@hotmail.com>
2021-11-17 22:20:28 +0000

    dataTypes

--- main.cpp
+++ main.cpp
@@ -8,5 +8,13 @@ int main() {
     int age = 43;
     cout << name << " is " << age << " years old" << endl;
 
+    //data types
+    char letter = 'A';
+    string sentence = "this is a string";
+    int wholeNumber = 43;
+    float decimal = 4.6;
+    double decimalWithMoreDigits = 3.446242902874;
+    bool boolean = true;
+
     return 0;
 }
